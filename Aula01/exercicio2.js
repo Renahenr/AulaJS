f5551258c4818ece8c7082cbf520bf54e4408593
Renahenr <Renanhenrique.fer@gmail.com>
2026-03-31 11:23:48 -0300

    30.3

--- Aula01/exercicio2.js
+++ Aula01/exercicio2.js
@@ -3,4 +3,4 @@ document.write("<h1>Aula 1 - Exercício 2</h1>");
 var fah = prompt("Digite a temp:");
 
 var resultado = Number(fah- 32) * 5/9;
-document.write("O resultado em celsios é "+ resultado + "<br>")
+document.write("O resultado em celsios é "+ resultado>") + "<br)
---
+++ Aula02/exercicio2.js
@@ -0,0 +1,12 @@
+function exercicio2()
+(
+  let fah = parseFloat(document.getElementById("fah").value);
+
+  let celsuis = (fah - 32) * 5 /9;
+
+  document.getElementById("resultado").innerHTML = " o valor em celsuis é: " + celsuis + ";
+
+
+
+)
+
