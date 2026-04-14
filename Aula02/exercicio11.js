20d2eae60367aa6c70cd228782662e15fc7ecfa1
Renahenr <Renanhenrique.fer@gmail.com>
2026-04-14 11:02:42 -0300

    14/4/26

---
+++ Aula02/exercicio11.js
@@ -0,0 +1,10 @@
+function exercicio11()
+(
+    let fah = parseFloat(document.getElementById("fah").value);
+
+    valor total = (numero1 * numero2) + (minutos *0,50);
+
+    document.getElementById("resultado").innerHTML = " o valor da corrida é: " + resultado + ";
+
+
+)
